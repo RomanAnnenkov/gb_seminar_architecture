1fd8598bf9ef0c5c174a32172c6ae9a94ce7875e
Roman Annenkov <roman.annenkov@gmail.com>
2023-10-03 12:37:48 +0300

    added getters to Ticket

--- lessonFour/src/main/java/saleTicketOnline/ticket/Ticket.java
+++ lessonFour/src/main/java/saleTicketOnline/ticket/Ticket.java
@@ -3,16 +3,26 @@ package saleTicketOnline.ticket;
 import java.time.LocalDateTime;
 
 public abstract class Ticket implements TicketActions {
-    public Ticket(int ticketNumber, LocalDateTime departureTime) {
-        this.ticketNumber = ticketNumber;
-        this.departureTime = departureTime;
-    }
-
     protected int ticketNumber;
     protected String name;
     protected String surname;
     protected String id;
     protected LocalDateTime departureTime;
+
+    public Ticket(int ticketNumber, LocalDateTime departureTime) {
+        this.ticketNumber = ticketNumber;
+        this.departureTime = departureTime;
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+
 
 
 

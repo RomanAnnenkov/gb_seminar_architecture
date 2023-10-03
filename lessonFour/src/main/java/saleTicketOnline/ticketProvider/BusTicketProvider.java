85a7bfca3ec8082608c6908bf4acb90bf558697f
Roman Annenkov <roman.annenkov@gmail.com>
2023-10-03 13:04:33 +0300

    added documentation to ticketProvider

--- lessonFour/src/main/java/saleTicketOnline/ticketProvider/BusTicketProvider.java
+++ lessonFour/src/main/java/saleTicketOnline/ticketProvider/BusTicketProvider.java
@@ -7,6 +7,10 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Класс поставщика автобусных билетов.
+ */
+
 public class BusTicketProvider implements TicketProviderActions {
 
     private final List<BusTicket> busTickets = new ArrayList<>();;
@@ -15,16 +19,33 @@ public class BusTicketProvider implements TicketProviderActions {
         busTickets.add(new BusTicket(1001, LocalDateTime.now(), 104, 30));
     }
 
+    /**
+     * Метод получает автобусные билеты доступные для бронирования.
+     * @param depTime время отправления в формате LocalDateTime
+     * @param route номер маршрута
+     * @return список доступных автобусных билетов
+     */
     @Override
     public List<Ticket> getAvailableTickets(LocalDateTime depTime, int route) {
         return null;
     }
 
+    /**
+     * метод бронирования билета
+     * @param ticket Билет с заполненными данными пассажира
+     * @return возвращает true в случае успешного бронировани, и false если операция не пройдёт
+     */
     @Override
     public boolean reserveTicket(Ticket ticket) {
         return false;
     }
 
+    /**
+     * Метод для отмены билета, при отмене информация о пассажире будет очищена,
+     *  и билет вернётся в список доступных для бронирования
+     * @param ticket билет с заполненными данными пассажира
+     * @return возвращает true в случае успешной отмены, и false если операция не пройдёт
+     */
     @Override
     public boolean canselTicket(Ticket ticket) {
         return false;
--- lessonFour/src/main/java/saleTicketOnline/ticketProvider/TicketProviderActions.java
+++ lessonFour/src/main/java/saleTicketOnline/ticketProvider/TicketProviderActions.java
@@ -6,8 +6,28 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TicketProviderActions {
+    /**
+     * Получение списка доступных для бронирования билетов
+     * @param depTime время отправления в формате LocalDateTime
+     * @param route номер маршрута
+     * @return возвращает список билетов
+     */
     List<Ticket> getAvailableTickets(LocalDateTime depTime, int route);
+
+    /**
+     * Бронирование билета, при бронировании он исключается из списка доступных для бронирования билетов
+     * и в билет вносятся данные пассажира
+     * @param ticket Билет с заполненными данными пассажира
+     * @return возвращает true если билет успешно забронировался, и false если нет.
+     */
     boolean reserveTicket(Ticket ticket);
+
+    /**
+     * отмена оформления билета, при этой операции у него очищаются данные пассажира,
+     * и он возвращается в список доступных для бронирования билетов
+     * @param ticket билет с заполненными данными пассажира
+     * @return возвращает true если билет успешно отменён, и false если нет.
+     */
     boolean canselTicket(Ticket ticket);
 
 }
